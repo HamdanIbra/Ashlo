aec8a4f52d8d44e991d8a8c50c4f6e4b4202e6a9
TaleenMusa <taleenkhoury20@gmail.com>
2023-04-04 14:53:37 +0300

    Added the Login and Regustration pages , urls and views

--- app1/views.py
+++ app1/views.py
@@ -1,8 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import *
+import bcrypt
+from django.contrib import messages
+
 def main(request):
     return render(request, 'main.html')
 
+def login(request):
+    return render(request, "login.html")
+
+
+def login_form(request):
+    errors = User.objects.basic_validator(request.POST)
+    # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        return redirect('/login')
+    email = request.POST['email']
+    password = request.POST['password']
+    user = User.objects.filter(email=email).first()
+    if user:
+        if bcrypt.checkpw(password.encode(), user.password.encode()):
+            request.session['user_id'] = user.id
+            return redirect('/')
+    messages.error(request, 'Invalid Credentials')
+    return redirect('/login')
+
+
+def register(request):
+    return render(request, "register.html")
+
+
+def registration(request):
+    errors = User.objects.basic_validator(request.POST)
+    # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        return redirect('/register')
+    else:
+        fname = request.POST['first_name']
+        lname = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        User.objects.create(first_name=fname, last_name=lname,email=email, password=hashed)
+        user = User.objects.last()
+        request.session['user_id'] = user.id
+    return redirect('/')
 
 
 # Create your views here.
